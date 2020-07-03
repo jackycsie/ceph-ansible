a78dc4738d5dd95d864c15c70e0772d930bd576d
Guillaume Abrioux <gabrioux@redhat.com>
2020-07-03 10:21:49 +0200

    ceph-crash: introduce new role ceph-crash
    This commit introduces a new role `ceph-crash` in order to deploy
    everything needed for the ceph-crash daemon.
    
    Signed-off-by: Guillaume Abrioux <gabrioux@redhat.com>

--- tests/functional/tests/mon/test_mons.py
+++ tests/functional/tests/mon/test_mons.py
@@ -40,3 +40,15 @@ class TestMons(object):
             if pattern.search(mon_initial_members_line) == None:  # noqa E501
                 result = False
                 assert result
+
+    @pytest.mark.no_docker
+    def test_ceph_crash_service_enabled_and_running(self, node, host):
+        s = host.service("ceph-crash")
+        assert s.is_enabled
+        assert s.is_running
+
+    @pytest.mark.docker
+    def test_ceph_crash_service_enabled_and_running_container(self, node, host):
+        s = host.service("ceph-crash@{hostname}".format(hostname=node["vars"]["inventory_hostname"]))
+        assert s.is_enabled
+        assert s.is_running
